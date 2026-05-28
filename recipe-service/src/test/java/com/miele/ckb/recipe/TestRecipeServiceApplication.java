9a80eb2bced713177135b951444147863537d451
Darius-DB <dariusboengiu@gmail.com>
2026-05-28 11:52:08 +0300

    bootsraped project

---
+++ recipe-service/src/test/java/com/miele/ckb/recipe/TestRecipeServiceApplication.java
@@ -0,0 +1,11 @@
+package com.miele.ckb.recipe;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestRecipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(RecipeServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
